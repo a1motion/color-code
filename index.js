0069c1a608f45edc1a50a936efe4990c80707512
Connor Love <connor@dotconnor.com>
2020-04-06 10:21:38 -0400

    update

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-control-regex */
-const escape = require(`escape-html`);
-const scanner = require(`./lib/scanner`);
-const parser = require(`./lib/parser`);
-const BASIC_COLORS = require(`./basic.json`);
+const escape = require("escape-html");
+const scanner = require("./lib/scanner");
+const parser = require("./lib/parser");
+const BASIC_COLORS = require("./basic.json");
 
 const fontColor = (color, options) =>
-  options.noHtml ? `` : `<span style="color:rgb(${color});">`;
+  options.noHtml ? "" : `<span style="color:rgb(${color});">`;
 const backgroundColor = (color, options) =>
-  options.noHtml ? `` : `<span style="background-color:rgb(${color});">`;
+  options.noHtml ? "" : `<span style="background-color:rgb(${color});">`;
 
 const checkRGBValue = (value) => {
   return (
@@ -20,7 +20,7 @@ const checkRGBValue = (value) => {
 
 const getColorFromParams = (params) => {
   const [type, r, g, b] = params;
-  if (type === `5` && checkRGBValue(r)) {
+  if (type === "5" && checkRGBValue(r)) {
     const id = Number(r);
     if (id < 8) {
       return BASIC_COLORS[id + 30];
@@ -29,7 +29,7 @@ const getColorFromParams = (params) => {
     if (id < 16) {
       return BASIC_COLORS[id + 90 - 8];
     }
-  } else if (type === `2`) {
+  } else if (type === "2") {
     if (checkRGBValue(r) && checkRGBValue(g) && checkRGBValue(b)) {
       return `${r},${g},${b}`;
     }
@@ -46,14 +46,14 @@ const colorCode = (str, opts) => {
   const options = Object.assign({}, defaultOptions, opts);
   let tokens = scanner(str);
   tokens = parser(tokens);
-  let s = ``;
+  let s = "";
   let RESETABLE = 0;
   let HAS_CHANGED_COLOR = false;
   let HAS_CHANGED_BACKGROUND = false;
   let LAST_COLOR_USED = null;
   const reset = (repeat = 1) => {
     if (!options.noHtml) {
-      s += `</span>`.repeat(repeat);
+      s += "</span>".repeat(repeat);
     }
   };
 
@@ -64,7 +64,7 @@ const colorCode = (str, opts) => {
       reset();
     }
 
-    LAST_COLOR_USED = [`fg`, color];
+    LAST_COLOR_USED = ["fg", color];
     HAS_CHANGED_COLOR = true;
     s += fontColor(color, options);
   };
@@ -76,20 +76,20 @@ const colorCode = (str, opts) => {
       reset();
     }
 
-    LAST_COLOR_USED = [`bg`, color];
+    LAST_COLOR_USED = ["bg", color];
     HAS_CHANGED_BACKGROUND = true;
     s += backgroundColor(color, options);
   };
 
   for (const token of tokens) {
-    if (token.type === `STRING`) {
+    if (token.type === "STRING") {
       s += options.noHtml ? token.value : escape(token.value);
-    } else if (token.type === `CONTROL`) {
+    } else if (token.type === "CONTROL") {
       const { parameters } = token.value;
       if (/^(3[0-7])$/.test(parameters[0])) {
         let [, color] = /^(3[0-7])$/.exec(parameters[0]);
         color = Number(color);
-        if (parameters[1] === `1`) {
+        if (parameters[1] === "1") {
           color += 60;
         }
 
@@ -97,43 +97,43 @@ const colorCode = (str, opts) => {
       } else if (/^(4[0-7])$/.test(parameters[0])) {
         let [, color] = /^(4[0-7])$/.exec(parameters[0]);
         color = Number(color) - 10;
-        if (parameters[1] === `1`) {
+        if (parameters[1] === "1") {
           color += 60;
         }
 
         setBackground(BASIC_COLORS[color]);
-      } else if (parameters[0] === `38`) {
+      } else if (parameters[0] === "38") {
         const color = getColorFromParams(parameters.slice(1));
         if (color) {
           setColor(color);
         }
-      } else if (parameters[0] === `39`) {
+      } else if (parameters[0] === "39") {
         if (HAS_CHANGED_COLOR) {
           reset();
-          if (LAST_COLOR_USED[0] && LAST_COLOR_USED[0] === `bg`) {
+          if (LAST_COLOR_USED[0] && LAST_COLOR_USED[0] === "bg") {
             RESETABLE--;
             setBackground(LAST_COLOR_USED[1]);
           }
         }
 
         HAS_CHANGED_COLOR = false;
-      } else if (parameters[0] === `48`) {
+      } else if (parameters[0] === "48") {
         const color = getColorFromParams(parameters.slice(1));
         if (color) {
           setBackground(color);
         }
-      } else if (parameters[0] === `49`) {
+      } else if (parameters[0] === "49") {
         if (HAS_CHANGED_BACKGROUND) {
           reset();
         }
 
-        if (LAST_COLOR_USED[0] && LAST_COLOR_USED[0] === `fg`) {
+        if (LAST_COLOR_USED[0] && LAST_COLOR_USED[0] === "fg") {
           RESETABLE--;
           setColor(LAST_COLOR_USED[1]);
         }
 
         HAS_CHANGED_BACKGROUND = false;
-      } else if (parameters[0] === `0` || parameters.length === 0) {
+      } else if (parameters[0] === "0" || parameters.length === 0) {
         HAS_CHANGED_BACKGROUND = false;
         HAS_CHANGED_COLOR = false;
         reset(RESETABLE);
@@ -146,10 +146,9 @@ const colorCode = (str, opts) => {
   return s;
 };
 
-Object.defineProperty(exports, `__esModule`, {
+Object.defineProperty(exports, "__esModule", {
   value: true,
 });
 
 module.exports = colorCode;
-
 exports.default = colorCode;
--- index.test.js
+++ index.test.js
@@ -1,121 +1,121 @@
-import test from "ava";
-import colorCode from "./index";
+const test = require("ava");
+const colorCode = require("./index");
 
-test(`basic red color`, (t) => {
+test("basic red color", (t) => {
   t.is(
-    colorCode(`\x1b[31mHelloWorld\x1b[0m`),
-    `<span style="color:rgb(222,56,43);">HelloWorld</span>`
+    colorCode("\x1b[31mHelloWorld\x1b[0m"),
+    '<span style="color:rgb(222,56,43);">HelloWorld</span>'
   );
 });
 
-test(`basic bright red color`, (t) => {
+test("basic bright red color", (t) => {
   t.is(
-    colorCode(`\x1b[31;1mHelloWorld\x1b[0m`),
-    `<span style="color:rgb(255,0,0);">HelloWorld</span>`
+    colorCode("\x1b[31;1mHelloWorld\x1b[0m"),
+    '<span style="color:rgb(255,0,0);">HelloWorld</span>'
   );
 });
 
-test(`basic red background color`, (t) => {
+test("basic red background color", (t) => {
   t.is(
-    colorCode(`\x1b[41mHelloWorld\x1b[0m`),
-    `<span style="background-color:rgb(222,56,43);">HelloWorld</span>`
+    colorCode("\x1b[41mHelloWorld\x1b[0m"),
+    '<span style="background-color:rgb(222,56,43);">HelloWorld</span>'
   );
 });
 
-test(`basic bright red background color`, (t) => {
+test("basic bright red background color", (t) => {
   t.is(
-    colorCode(`\x1b[41;1mHelloWorld\x1b[0m`),
-    `<span style="background-color:rgb(255,0,0);">HelloWorld</span>`
+    colorCode("\x1b[41;1mHelloWorld\x1b[0m"),
+    '<span style="background-color:rgb(255,0,0);">HelloWorld</span>'
   );
 });
 
-test(`resets with nothing`, (t) => {
+test("resets with nothing", (t) => {
   t.is(
-    colorCode(`\x1b[31mHelloWorld`),
-    `<span style="color:rgb(222,56,43);">HelloWorld</span>`
+    colorCode("\x1b[31mHelloWorld"),
+    '<span style="color:rgb(222,56,43);">HelloWorld</span>'
   );
 });
 
-test(`resets with empty reset tag`, (t) => {
+test("resets with empty reset tag", (t) => {
   t.is(
-    colorCode(`\x1b[31mHelloWorld\x1b[m`),
-    `<span style="color:rgb(222,56,43);">HelloWorld</span>`
+    colorCode("\x1b[31mHelloWorld\x1b[m"),
+    '<span style="color:rgb(222,56,43);">HelloWorld</span>'
   );
 });
 
-test(`resets with one`, (t) => {
+test("resets with one", (t) => {
   t.is(
-    colorCode(`\u001b[40m A \u001b[41m B \u001b[42m C \u001b[43m D \u001b[0m`),
-    `<span style="background-color:rgb(1,1,1);"> A </span><span style="background-color:rgb(222,56,43);"> B </span><span style="background-color:rgb(57,181,74);"> C </span><span style="background-color:rgb(255,199,6);"> D </span>`
+    colorCode("\u001b[40m A \u001b[41m B \u001b[42m C \u001b[43m D \u001b[0m"),
+    '<span style="background-color:rgb(1,1,1);"> A </span><span style="background-color:rgb(222,56,43);"> B </span><span style="background-color:rgb(57,181,74);"> C </span><span style="background-color:rgb(255,199,6);"> D </span>'
   );
 });
 
-test(`only reset color`, (t) => {
+test("only reset color", (t) => {
   t.is(
-    colorCode(`\u001b[31mA\u001b[41mB\u001b[39mRed`),
-    `<span style="color:rgb(222,56,43);">A<span style="background-color:rgb(222,56,43);">B</span></span><span style="background-color:rgb(222,56,43);">Red</span>`
+    colorCode("\u001b[31mA\u001b[41mB\u001b[39mRed"),
+    '<span style="color:rgb(222,56,43);">A<span style="background-color:rgb(222,56,43);">B</span></span><span style="background-color:rgb(222,56,43);">Red</span>'
   );
 });
 
-test(`only reset background-color`, (t) => {
+test("only reset background-color", (t) => {
   t.is(
-    colorCode(`\u001b[41mA\u001b[31mB\u001b[49mRed`),
-    `<span style="background-color:rgb(222,56,43);">A<span style="color:rgb(222,56,43);">B</span></span><span style="color:rgb(222,56,43);">Red</span>`
+    colorCode("\u001b[41mA\u001b[31mB\u001b[49mRed"),
+    '<span style="background-color:rgb(222,56,43);">A<span style="color:rgb(222,56,43);">B</span></span><span style="color:rgb(222,56,43);">Red</span>'
   );
 });
 
-test(`24-bit colors`, (t) => {
+test("24-bit colors", (t) => {
   t.is(
-    colorCode(`\u001b[38;2;12;34;56mTest`),
-    `<span style="color:rgb(12,34,56);">Test</span>`
+    colorCode("\u001b[38;2;12;34;56mTest"),
+    '<span style="color:rgb(12,34,56);">Test</span>'
   );
 });
 
-test(`look by id with simple colors`, (t) => {
+test("look by id with simple colors", (t) => {
   t.is(
-    colorCode(`\u001b[38;5;3mTest`),
-    `<span style="color:rgb(255,199,6);">Test</span>`
+    colorCode("\u001b[38;5;3mTest"),
+    '<span style="color:rgb(255,199,6);">Test</span>'
   );
 });
 
-test(`look by id with bright colors`, (t) => {
+test("look by id with bright colors", (t) => {
   t.is(
-    colorCode(`\u001b[38;5;9mTest`),
-    `<span style="color:rgb(255,0,0);">Test</span>`
+    colorCode("\u001b[38;5;9mTest"),
+    '<span style="color:rgb(255,0,0);">Test</span>'
   );
 });
 
-test(`look by id with simple background-colors`, (t) => {
+test("look by id with simple background-colors", (t) => {
   t.is(
-    colorCode(`\u001b[48;5;3mTest`),
-    `<span style="background-color:rgb(255,199,6);">Test</span>`
+    colorCode("\u001b[48;5;3mTest"),
+    '<span style="background-color:rgb(255,199,6);">Test</span>'
   );
 });
 
-test(`look by id with bright background-colors`, (t) => {
+test("look by id with bright background-colors", (t) => {
   t.is(
-    colorCode(`\u001b[48;5;9mTest`),
-    `<span style="background-color:rgb(255,0,0);">Test</span>`
+    colorCode("\u001b[48;5;9mTest"),
+    '<span style="background-color:rgb(255,0,0);">Test</span>'
   );
 });
 
-test(`return nothing when its a incorrect id type`, (t) => {
-  t.is(colorCode(`\u001b[38;3;9mTest`), `Test`);
+test("return nothing when its a incorrect id type", (t) => {
+  t.is(colorCode("\u001b[38;3;9mTest"), "Test");
 });
 
-test(`respects no html options background`, (t) => {
+test("respects no html options background", (t) => {
   t.is(
-    colorCode(`\u001b[48;5;9mTest`, {
+    colorCode("\u001b[48;5;9mTest", {
       noHtml: true,
     }),
-    `Test`
+    "Test"
   );
 });
-test(`respects no html options foreground`, (t) => {
+test("respects no html options foreground", (t) => {
   t.is(
-    colorCode(`\u001b[38;5;9mTest`, {
+    colorCode("\u001b[38;5;9mTest", {
       noHtml: true,
     }),
-    `Test`
+    "Test"
   );
 });
